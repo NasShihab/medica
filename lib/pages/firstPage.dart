4ec28c8225c1786d13f0523980f5bd8a9ac61453
NAS <nas.shihab92@gmail.com>
2022-08-14 00:03:09 +0600

    rename & sorting 2.0

--- lib/pages/firstPage.dart
+++ lib/pages/firstPage.dart
@@ -1,12 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'login/login.dart';
-import 'login/welcome.dart';
-import 'signUp/create_pin.dart';
-import 'signUp/fill_profile.dart';
-import 'signUp/fingerprint.dart';
-import 'signUp/sign_up.dart';
-
 // ignore: camel_case_types
 class firstPage extends StatelessWidget {
   const firstPage({Key? key}) : super(key: key);
@@ -25,54 +18,42 @@ class firstPage extends StatelessWidget {
               children: [
                 TextButton(
                     onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (BuildContext context) =>
-                              const welcome()));
+                      Navigator.pushNamed(context, '/welcome');
                     },
                     child: Text('9_Light_lets you in',style: TextStyle(
                       fontSize: 30.sp,
                     ),)),
                 TextButton(
                     onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (BuildContext context) =>
-                          const sign_up()));
+                      Navigator.pushNamed(context, '/sign_up');
                     },
                     child: Text('10_Light_sign up blank form',style: TextStyle(
                       fontSize: 30.sp,
                     ),)),
                 TextButton(
                     onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (BuildContext context) =>
-                          const login()));
+                      Navigator.pushNamed(context, '/login');
                     },
                     child: Text('13_Light_sign in blank form',style: TextStyle(
                       fontSize: 30.sp,
                     ),)),
                 TextButton(
                     onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (BuildContext context) =>
-                          const fill_profile()));
+                      Navigator.pushNamed(context, '/fill_profile');
                     },
                     child: Text('16_Light_fill your profile blank form',style: TextStyle(
                       fontSize: 30.sp,
                     ),)),
                 TextButton(
                     onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (BuildContext context) =>
-                          const create_pin()));
+                      Navigator.pushNamed(context, '/create_pin');
                     },
                     child: Text('18_Light_create new PIN',style: TextStyle(
                       fontSize: 30.sp,
                     ),)),
                 TextButton(
                     onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (BuildContext context) =>
-                          const fingerprint()));
+                      Navigator.pushNamed(context, '/fingerprint');
                     },
                     child: Text('19_Light_set your fingerprint',style: TextStyle(
                       fontSize: 30.sp,
--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:medica/pages/login/login.dart';
+import 'package:medica/pages/login/welcome.dart';
+import 'package:medica/pages/signUp/create_pin.dart';
+import 'package:medica/pages/signUp/fill_profile.dart';
+import 'package:medica/pages/signUp/fingerprint.dart';
+import 'package:medica/pages/signUp/sign_up.dart';
 
 import 'firstPage.dart';
 
@@ -13,9 +19,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenUtilInit(
       builder: (_, child) => MaterialApp(
+        initialRoute: '/',
+        routes: {
+          '/' : (context) => const firstPage(),
+          '/welcome' : (context) => const welcome(),
+          '/login' : (context) => const login(),
+          '/sign_up' : (context) => const sign_up(),
+          '/fill_profile' : (context) => const fill_profile(),
+          '/fingerprint' : (context) => const fingerprint(),
+          '/create_pin' : (context) => const create_pin(),
+        },
         theme: ThemeData(fontFamily: 'Roboto'),
         debugShowCheckedModeBanner: false,
-        home: const firstPage(),
       ),
       designSize: const Size(428, 926),
     );
